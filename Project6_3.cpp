3bb93f8a4c0b38e8e4f643481d8e79b67f422259
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-04-02 19:42:46 +0600

    Update Project6_3.cpp

--- Project6_3.cpp
+++ Project6_3.cpp
@@ -1,35 +1,25 @@
-﻿#include <iostream>
-#include <vector>
+#include <iostream>
 #include <Windows.h>
 
-void calcResult(int number)
+int calcResult(int count)
 {
-	double numberFib = 0;
-
-	double num0 = 0;
-	double num1 = 1;
-
-	std::cout << "Числа Фибоначчи: ";
-
-	for (int i = 0; i < number; i++)
-	{
-		std::cout << num0 << " ";
-		numberFib = num0 + num1;
-		num0 = num1;
-		num1 = numberFib;
-	}
+    if (count <= 1) return count;
+    return calcResult(count - 1) + calcResult(count - 2);
 }
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
 	int number = 0;
 
 	std::cout << "Введите число: ";
 	std::cin >> number;
 
-	calcResult(number);
+	std::cout << "Числа Фибоначчи: ";
 
+    for (int i = 0; i < number; i++) {
+        std::cout << calcResult(i) << " ";
+    }
+	return 0;
 }
